b3744737e0ae44a823662c7b43e35d6a3042304b
engr425imran <fana9994@yahoo.com>
2023-11-23 12:55:06 +0500

    removed zip file

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ use App\User;
 Route::get('/', function () {
     $user = User::find(1)->toArray();
     // print_r($user['email']);die;
-    Mail::send('emails.test', $user, function($message) use ($user) {
-        $message->to('zubairbasharat14@gmail.com');
-        $message->subject('Sendgrid Testing');
-    });
+    // Mail::send('emails.test', $user, function ($message) use ($user) {
+    // $message->to('zubairbasharat14@gmail.com');
+    // $message->subject('Sendgrid Testing');
+    // });
     return view('admin.login');
 });
 // Route::get('/admin/vendor', function () {
@@ -56,15 +56,15 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::post('login', [AdminController::class, 'login']);
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/admin/dashboard',[AdminController::class, 'dashboard']);
-    Route::get('/admin/users/manager',[AdminController::class, 'managers']);
-    Route::get('/admin/users/valet',[AdminController::class, 'valets']);
-    Route::get('/admin/users/customer',[AdminController::class, 'customers']);
-    Route::get('/admin/cms/about',[AdminController::class, 'about']);
-    Route::get('/admin/cms/faq',[AdminController::class, 'faq']);
-    Route::get('/admin/cms/terms',[AdminController::class, 'terms']);
-    Route::get('/admin/setting',[AdminController::class, 'setting']);
-    Route::get('/admin/profile',[AdminController::class, 'profile']);
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
+    Route::get('/admin/users/manager', [AdminController::class, 'managers']);
+    Route::get('/admin/users/valet', [AdminController::class, 'valets']);
+    Route::get('/admin/users/customer', [AdminController::class, 'customers']);
+    Route::get('/admin/cms/about', [AdminController::class, 'about']);
+    Route::get('/admin/cms/faq', [AdminController::class, 'faq']);
+    Route::get('/admin/cms/terms', [AdminController::class, 'terms']);
+    Route::get('/admin/setting', [AdminController::class, 'setting']);
+    Route::get('/admin/profile', [AdminController::class, 'profile']);
     Route::get('delete/user/{id}', [AdminController::class, 'delete']);
     Route::post('save-about', [AdminController::class, 'save_about']);
     Route::post('save-faq', [AdminController::class, 'save_faq']);
